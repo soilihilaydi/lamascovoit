0fa97cd83d84c5dc6bd6bc9d31c7f2b04517097d
soilihi laydi <soilihi.laydi@gmail.com>
2024-05-06 18:20:02 +0200

    fix probleme fichier gitignore

---
+++ api/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+
+const app = express();
+
+// Middleware pour parser le corps des requêtes en JSON
+app.use(express.json());
+
+// Routes
+
+
+export default app;
---
+++ api/server.js
@@ -0,0 +1,21 @@
+import app from "./app.js";
+import sequelize from "./src/config/db.config.js";  
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('hey man la Connexion à la base de données ben ca roule man');
+        // Écoute du serveur sur le port spécifié seulement après la connexion réussie à la base de données
+        app.listen(PORT, () => {
+            console.log(`Hey man ça roule pour toi pour moi ça tourne le serveur et prêt sur les ondes ${PORT}`);
+        });
+    })
+    .catch(err => {
+        // Log des erreurs si la connexion à la base de données échoue
+        console.error('Impossible de se connecter à la base de données:', err);
+    });
