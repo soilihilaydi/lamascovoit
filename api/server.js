da9b4a695a6a29d53023703a7861c8b54741dc2a
soilihi laydi <soilihi.laydi@gmail.com>
2024-05-06 13:17:52 +0200

    mise en place du serveur creation des tables et connection de la base de donnée a l'api

--- api/server.js
+++ api/server.js
@@ -9,10 +9,10 @@ const PORT = process.env.PORT;
 // Tentez d'authentifier et de connecter Sequelize à votre base de données
 sequelize.authenticate()
     .then(() => {
-        console.log('Connexion à la base de données réussie');
+        console.log('hey man la Connexion à la base de données ben ca roule man');
         // Écoute du serveur sur le port spécifié seulement après la connexion réussie à la base de données
         app.listen(PORT, () => {
-            console.log(`Hey man ça roule pour toi pour moi ça tourne le serveur et prêt ${PORT}`);
+            console.log(`Hey man ça roule pour toi pour moi ça tourne le serveur et prêt sur les ondes ${PORT}`);
         });
     })
     .catch(err => {
