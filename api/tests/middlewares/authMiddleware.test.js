01f804c1d2eed4f0ca2899212a4678df23ba54be
soilihi laydi <soilihi.laydi@gmail.com>
2024-05-15 11:25:50 +0200

    authentification et profile utilisateur avec test unitaires done

---
+++ api/tests/middlewares/authMiddleware.test.js
@@ -0,0 +1,56 @@
+import jwt from 'jsonwebtoken';
+import authMiddleware from '../../src/middlewares/authMiddleware.js';
+
+jest.mock('jsonwebtoken');
+jest.mock('../../src/models/utilisateurModel.js', () => ({
+  findById: jest.fn()
+}));
+
+describe('Auth Middleware', () => {
+  let mockReq;
+  let mockRes;
+  let mockNext;
+
+  beforeEach(() => {
+    mockReq = {
+      headers: {
+        authorization: 'Bearer validToken'
+      }
+    };
+    mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    mockNext = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Jeton JWT valide', async () => {
+    const mockUser = { _id: 'mockUserId' };
+    const mockDecodedToken = { idUtilisateur: mockUser._id };
+
+    jwt.verify.mockReturnValueOnce(mockDecodedToken);
+    const mockUtilisateurModel = require('../../src/models/utilisateurModel.js');
+    mockUtilisateurModel.findById.mockResolvedValueOnce(mockUser);
+
+    await authMiddleware(mockReq, mockRes, mockNext);
+
+    expect(mockReq.user).toEqual(mockUser);
+    expect(mockNext).toHaveBeenCalled();
+  });
+
+  it('Jeton JWT invalide', async () => {
+    jwt.verify.mockImplementationOnce(() => {
+      throw new Error('Invalid token');
+    });
+
+    await authMiddleware(mockReq, mockRes, mockNext);
+
+    expect(mockRes.status).toHaveBeenCalledWith(401);
+    expect(mockRes.json).toHaveBeenCalledWith({ message: 'Jeton JWT invalide ou expiré' });
+    expect(mockNext).not.toHaveBeenCalled();
+  });
+});
