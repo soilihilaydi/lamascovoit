42a62b13278ee7d387b09f47d1dff8531a08a4d4
soilihi laydi <soilihi.laydi@gmail.com>
2024-09-17 03:52:27 +0200

    ajout de swagger et correction des test

--- api/tests/unitaires/models/reservationModel.test.js
+++ api/tests/unitaires/models/reservationModel.test.js
@@ -1,50 +1,40 @@
-import { Sequelize } from 'sequelize';
-import Reservation from '../../../src/models/reservationModel.js';
 import dotenv from 'dotenv';
+dotenv.config();
+process.env.NODE_ENV = 'test';
 
-dotenv.config(); // Charger les variables d'environnement
+import sequelize from '../../../src/config/db.config.js';
+import models from '../../../src/models/index.js';
 
-// Initialisation de Sequelize avec les paramètres MySQL
-const sequelizeInstance = new Sequelize(
-  process.env.TEST_DB_NAME,
-  process.env.TEST_DB_USER,
-  process.env.TEST_DB_PASS,
-  {
-    host: process.env.TEST_DB_HOST,
-    dialect: process.env.TEST_DB_DIALECT,
-    logging: false, // Désactiver les logs SQL pour les tests
-  }
-);
+console.log('models:', models); // Pour vérifier le contenu de models
 
-const ReservationModel = Reservation(sequelizeInstance);
+const { Reservation } = models;
 
 describe('Reservation Model', () => {
   beforeAll(async () => {
     try {
-      await sequelizeInstance.authenticate();
+      await sequelize.authenticate();
       console.log('La connexion à la base de données a été établie avec succès.');
 
-      // Désactiver les contraintes de clé étrangère temporairement
-      await sequelizeInstance.query('SET FOREIGN_KEY_CHECKS = 0');
-      await sequelizeInstance.sync({ force: true });
-      await sequelizeInstance.query('SET FOREIGN_KEY_CHECKS = 1');
-    } catch (error) { 
+      await sequelize.query('SET FOREIGN_KEY_CHECKS = 0');
+      await sequelize.sync({ force: true });
+      await sequelize.query('SET FOREIGN_KEY_CHECKS = 1');
+    } catch (error) {
       console.error('Impossible de se connecter à la base de données :', error);
     }
   });
 
   test('devrait avoir un nom de modèle et des propriétés corrects', () => {
-    expect(ReservationModel.tableName).toBe('Reservations');
-    expect(ReservationModel.rawAttributes.idReservation.type.key).toBe('INTEGER');
-    expect(ReservationModel.rawAttributes.idUtilisateur.type.key).toBe('INTEGER');
-    expect(ReservationModel.rawAttributes.idTrajet.type.key).toBe('INTEGER');
+    expect(Reservation.tableName).toBe('Reservations');
+    expect(Reservation.rawAttributes.idReservation.type.key).toBe('INTEGER');
+    expect(Reservation.rawAttributes.idUtilisateur.type.key).toBe('INTEGER');
+    expect(Reservation.rawAttributes.idTrajet.type.key).toBe('INTEGER');
   });
 
   test('devrait être initialisé avec les propriétés correctes', () => {
-    const reservation = ReservationModel.build({
+    const reservation = Reservation.build({
       idUtilisateur: 1,
       idTrajet: 1,
-      DateReservation: new Date()
+      DateReservation: new Date(),
     });
 
     expect(reservation.idUtilisateur).toBe(1);
